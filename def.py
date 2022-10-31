db005911bdf128876e7c05a13219268292869c1a
Yanvi09 <116386502+Yanvi09@users.noreply.github.com>
2022-10-31 21:56:52 +0530

    Create def.py

---
+++ def.py
@@ -0,0 +1,16 @@
+# Python3 code to demonstrate
+# def keyword
+
+# function for subtraction of 2 numbers.
+def subNumbers(x, y):
+	return (x-y)
+
+# main code
+a = 90
+b = 50
+
+# finding subtraction
+result = subNumbers(a, b)
+
+# print statement
+print("subtraction of ", a, " and ", b, " is = ", result)
